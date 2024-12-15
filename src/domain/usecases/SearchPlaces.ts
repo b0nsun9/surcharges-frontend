9993a88aca77bee1090e79a35e314eae4ade025c
Bonsung Koo <developer@bonsung.me>
2024-12-16 09:16:36 +1300

    List of places has been implemented #1

--- src/domain/usecases/SearchPlaces.ts
+++ src/domain/usecases/SearchPlaces.ts
@@ -1,17 +1,25 @@
-export default async function SearchPlaces(searchText: string) {
+export default async function SearchPlaces(searchText: string, nextPageToken?: string) {
 
   console.log('searchFor: ', searchText)
-  
+  console.log('nextPageToken: ', nextPageToken)
+
+  const body: string = nextPageToken && nextPageToken != ''
+    ? JSON.stringify({
+      'textQuery': searchText,
+      'pageToken': nextPageToken
+    })
+    : JSON.stringify({
+      'textQuery': searchText,
+    })
+
   const response = await fetch('https://places.googleapis.com/v1/places:searchText', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'X-Goog-Api-Key': import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-      'X-Goog-FieldMask': "places.id,places.displayName,places.formattedAddress",
+      'X-Goog-FieldMask': "nextPageToken,places.id,places.displayName,places.formattedAddress,places.addressComponents",
     },
-    body: JSON.stringify({
-      'textQuery': searchText,
-    })
+    body: body
   })
 
   if (!response.ok) {
---
+++ src/presentation/components/placeList/PlaceList.tsx
@@ -0,0 +1,62 @@
+/* styles */
+import styles from './PlaceList.module.css'
+
+/* frameworks */
+
+/* components */
+
+/* usecases */
+import MakeAddress from '@usecases/MakeAddress'
+
+interface PlacesListProps {
+  places: []
+  selectedPlace: (id: string) => void
+}
+
+export default function PlacesList({ places, selectedPlace }: PlacesListProps) {
+
+  const handleOnClick = (id: string) => {
+    selectedPlace(id)
+  }
+
+  return (
+    <div>
+      {
+        places && places.map((place: any) => (
+          <Place
+            place={place}
+            key={place.id}
+            onClick={(id) =>
+              handleOnClick(id)
+            }
+          />
+        ))
+      }
+    </div>
+  )
+}
+
+interface PlaceProps {
+  place: any
+  onClick: (id: string) => void
+}
+
+function Place({ place, onClick }: PlaceProps) {
+  return (
+    <div
+      className={styles['place-wrapper']}
+      onClick={() => onClick(place.id)}
+    >
+      <div className={styles['place-name']}>
+        <p>{place.displayName.text}</p>
+      </div>
+      <div className={styles['place-address']}>
+        <p>
+          {
+            MakeAddress(place.addressComponents, place.formattedAddress)
+          }
+        </p>
+      </div>
+    </div>
+  )
+}
